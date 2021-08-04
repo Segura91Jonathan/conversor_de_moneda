b141d9406e575d99b8764f0ecd82e111425b96e7
Segura91Jonathan <j.f.segura91@gmail.com>
2021-08-04 15:09:04 -0300

    desafio de platzi para hacer la operacion inversa

--- conversor.py
+++ conversor.py
@@ -5,3 +5,13 @@ dolares = pesos / valor_dolar
 dolares = round(dolares,2)
 dolares = str(dolares)
 print("tenes $" + dolares + " dolares")
+
+
+dolares1 = input("ingrese su cantidad de dolares: ")
+dolares1 = float(dolares1)
+valor_peso = 190
+pesos1 = dolares1 * valor_peso
+pesos1 = round(pesos1,2)
+pesos1 = str(pesos1)
+print("tenes $" + pesos1 + " pesos")
+
