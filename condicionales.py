ab12ea10eb19435193a5db3f5244c67ddefa4ec7
Segura91Jonathan <j.f.segura91@gmail.com>
2021-08-04 16:08:22 -0300

    cambios siguiente ejemplo

--- condicionales.py
+++ condicionales.py
@@ -1,6 +1,16 @@
-edad = int( input("escribe tu edad:"))
+# edad = int( input("escribe tu edad:"))
 
-if edad >= 18:
-    print("eres mayor de edad")
+# if edad >= 18:
+#     print("eres mayor de edad")
+# else:
+#     print("es menor de edad")
+
+
+numero = int(input('write a number:'))
+
+if numero > 5:
+    print("es mayor a 5")
+elif numero == 5:
+    print("es igual a 5")
 else:
-    print("es menor de edad")
+    print("es menor a 5")   
